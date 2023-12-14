467091c8b77e7c989b50d526a1e6a0a1d0686e22
abhaynavada <abhaynavada@gmail.com>
2023-12-14 10:21:30 -0800

    Completed Hard Assignments - Week 1 - JavaScript

--- Assignments/Week 1 - Javascript/hard/calculator.js
+++ Assignments/Week 1 - Javascript/hard/calculator.js
@@ -16,6 +16,50 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num == 0) {
+      throw new Error();
+    }
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(arithString) {
+    
+    let cleanedArithString = arithString.replace(/\s+/g, '').trim();
+    let regex = /^[0-9+\-*/(). ]+$/;
+
+    if (!regex.test(cleanedArithString) || cleanedArithString.includes('/0')) {
+      throw new Error();
+    }
+
+    this.result = eval(cleanedArithString);
+  }
+}
 
 module.exports = Calculator;
